e3e491ee604daaaad263d799a5a9c188aca0c762
Peter Kosztolanyi <peter.kosztolanyi@gmail.com>
2022-08-21 13:02:59 +0200

    Add another dummy dag

---
+++ dags/public_hello.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+
+def print_hello():
+    return 'Public Hello world from public Airflow DAG!'
+
+dag = DAG('public_hello', description='Public Hello World DAG',
+          schedule_interval='0 12 * * *',
+          start_date=datetime(2017, 3, 20), catchup=False)
+
+hello_operator = PythonOperator(task_id='public_hello_task', python_callable=print_hello, dag=dag)
+
+hello_operator
